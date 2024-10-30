efc8f789f268f661c3c9d31cca7ae7ceb84d9edc
Preranabrainvire <preranababar9403@gmail.com>
2024-10-30 11:01:47 +0530

    collection text responsive

--- src/components/common/Shop.jsx
+++ src/components/common/Shop.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import React from "react";
 
-const Shop = ({data, row}) => {
+const Shop = ({ data, row }) => {
   return (
-    <div className={`max-width flex gap-5 max-md:flex-col ${row && "flex-row-reverse"}`}>
+    <div
+      className={`max-width flex gap-5 max-md:flex-col ${
+        row && "flex-row-reverse"
+      }`}
+    >
       <div className="bg-black w-1/2 py-5 px-2 max-md:w-full">
         <div className="flex justify-center items-center h-[80%]">
-          <img
-            src={data[0].img}
-            alt=""
-            className="object-cover w-[80%] mb-5"
-          />
+          <img src={data[0].img} alt="" className="object-cover w-[80%] mb-5" />
         </div>
 
         <div className="text-white px-10 pb-10">
@@ -19,7 +19,7 @@ const Shop = ({data, row}) => {
           <div className="flex gap-2 items-center">
             <Link
               href="/"
-              className="text-xl max-md:text-lg underline underline-offset-4"
+              className="text-xl max-md:text-xs underline underline-offset-4"
             >
               Collections
             </Link>
@@ -37,13 +37,13 @@ const Shop = ({data, row}) => {
         <div className="bg-[#F3F6F7] flex gap-10 py-5 px-5 justify-between items-end">
           <div className="w-[100px]">
             <h2 className="text-3xl pb-2 lg:text-nowrap max-md:text-lg ">
-          {data[1].title}
+              {data[1].title}
             </h2>
 
             <div className="flex gap-2 items-center">
               <Link
                 href="/"
-                className="text-xl max-md:text-lg underline underline-offset-4"
+                className="text-xl max-md:text-xs underline underline-offset-4"
               >
                 Collections
               </Link>
@@ -67,13 +67,13 @@ const Shop = ({data, row}) => {
         <div className="bg-[#F3F6F7] flex gap-10 py-5 px-5 justify-between items-end">
           <div>
             <h2 className="text-3xl pb-2 lg:text-nowrap max-md:text-lg">
-            {data[2].title}
+              {data[2].title}
             </h2>
 
             <div className="flex gap-2 items-center">
               <Link
                 href="/"
-                className="text-xl max-md:text-lg underline underline-offset-4"
+                className="text-xl max-md:text-xs underline underline-offset-4"
               >
                 Collections
               </Link>
